171d6056d31a102e0f47d58c689abc445cdbdc0c
Stiliyan St <65528270+stiliyan172@users.noreply.github.com>
2022-03-21 21:21:25 +0200

    Add files via upload

---
+++ 01_take_skip.py
@@ -0,0 +1,27 @@
+class take_skip:
+    def __init__(self, step: int, count: int):
+        self.step = step
+        self.count = count
+        self.i = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.count > 0:
+            result = self.i
+            self.i += self.step
+            self.count -= 1
+            return result
+
+        else:
+            raise StopIteration()
+
+
+numbers = take_skip(2, 6)
+for number in numbers:
+    print(number)
+
+numbers = take_skip(10, 5)
+for number in numbers:
+    print(number)
